cb541394caf15988428ac74645875c9d989c7f0b
ryanjm06 <kgq511@york.ac.uk>
2025-10-10 19:21:24 +0100

    Fixed some bugs

--- sketch.js
+++ sketch.js
@@ -1,24 +1,30 @@
 const GRID_SIZE = 20;
+let width
+let numberOfCells = width/GRID_SIZE;
+let randomCellx;
+let randomCelly;
+let randomCellFloorx;
+let randomCellFloory;
+let foodX;
+let foodY;
+let headX = 20;
+let headY = 300;
 
 function setup() {
     createCanvas(600,600);
     frameRate(5);
-    let numberOfCells = width/GRID_SIZE;
-    let randomCellx = random(numberOfCells);
-    let randomCellFloorx = floor(randomCellx);
-    let randomCelly = random(numberOfCells);
-    let randomCellFloory = floor(randomCelly);
-    let foodX;
-    let foodY;
+    randomCellx = random(numberOfCells);
+    randomCellFloorx = floor(randomCellx);
+    randomCelly = random(numberOfCells);
+    randomCellFloory = floor(randomCelly);
 }
 
-    let headX = 20;
-    let headY = 300;
+
+
 function draw() {
     background(255)
     if (headX > 590) {
-    }
-    else {
+    } else {
     fill(0,200,0);
     square(headX,headY,GRID_SIZE)
     headX = headX + 20
