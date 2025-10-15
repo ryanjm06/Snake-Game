87c5336d0c90ef0f5af8febf44abeb6cbd0b4090
ryanjm06 <kgq511@york.ac.uk>
2025-10-15 11:31:41 +0100

    Completed Stage 1

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
 const GRID_SIZE = 20;
-let width
-let numberOfCells = width/GRID_SIZE;
+let numberOfCells = 600/GRID_SIZE;
 let randomCellx;
 let randomCelly;
 let randomCellFloorx;
@@ -17,20 +16,23 @@ function setup() {
     randomCellFloorx = floor(randomCellx);
     randomCelly = random(numberOfCells);
     randomCellFloory = floor(randomCelly);
+    foodY = randomCellFloory * GRID_SIZE + GRID_SIZE / 2;
+    foodX = randomCellFloorx * GRID_SIZE + GRID_SIZE / 2;
 }
 
 
 
 function draw() {
     background(255)
-    if (headX > 590) {
+    if (headX > 560) {
+        fill(0,200,0);
+        square(headX,headY,GRID_SIZE)
+        headX = headX
     } else {
-    fill(0,200,0);
-    square(headX,headY,GRID_SIZE)
-    headX = headX + 20
+        fill(0,200,0);
+        square(headX,headY,GRID_SIZE)
+        headX = headX + GRID_SIZE
     }
-    foodY = randomCellFloory * GRID_SIZE + GRID_SIZE / 2;
-    foodX = randomCellFloorx * GRID_SIZE + GRID_SIZE / 2;
     fill(200,0,0)
     circle(foodX,foodY,GRID_SIZE)
 }
