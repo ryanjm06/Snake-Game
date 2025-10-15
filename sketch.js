f73f0b3f3ad02383310f169cd8de9fcc52ebbbfe
ryanjm06 <kgq511@york.ac.uk>
2025-10-15 12:02:46 +0100

    Completed Stage 2

--- sketch.js
+++ sketch.js
@@ -8,6 +8,8 @@ let foodX;
 let foodY;
 let headX = 20;
 let headY = 300;
+let DirX = 20
+let DirY = 0
 
 function setup() {
     createCanvas(600,600);
@@ -24,15 +26,36 @@ function setup() {
 
 function draw() {
     background(255)
-    if (headX > 560) {
+    fill(200,0,0)
+    circle(foodX,foodY,GRID_SIZE)
+    if (headX > 560 || headX < 20 || headY > 560 || headY < 40) {
         fill(0,200,0);
         square(headX,headY,GRID_SIZE)
         headX = headX
+        headY = headY
     } else {
         fill(0,200,0);
         square(headX,headY,GRID_SIZE)
-        headX = headX + GRID_SIZE
+        headX = headX + DirX
+        headY = headY + DirY
     }
-    fill(200,0,0)
-    circle(foodX,foodY,GRID_SIZE)
+}
+
+function keyPressed() {
+    if (keyCode === UP_ARROW) {
+        DirX = 0
+        DirY = -20
+    }
+    if (keyCode === LEFT_ARROW) {
+        DirY = 0
+        DirX = -20
+    }
+    if (keyCode === DOWN_ARROW) {
+        DirX = 0
+        DirY = 20
+    }
+    if (keyCode === RIGHT_ARROW) {
+        DirY = 0
+        DirX = 20
+    }
 }
